83d473b23c304f1e2fc04c40bc713105bd1ae574
Isaiah <isaiah.leiva@gmail.com>
2022-03-02 14:45:26 -0700

    commit

--- server/models/Galaxy.js
+++ server/models/Galaxy.js
@@ -2,5 +2,11 @@ import { Schema } from "mongoose";
 
 
 export const GalaxySchema = new Schema({
+    name: { type: String, required: true },
+    //TODO: add more properties
+},
+    //NOTE: Explain the line below
+    { timestamps: true, toJSON: { virtuals: true } }
+)
 
-})
+GalaxySchema.virtual('')
