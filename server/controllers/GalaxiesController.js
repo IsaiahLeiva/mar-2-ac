a23e89b8d4d33532205f23e647aa10965a3877f1
Isaiah <isaiah.leiva@gmail.com>
2022-03-02 14:59:40 -0700

    commit

--- server/controllers/GalaxiesController.js
+++ server/controllers/GalaxiesController.js
@@ -7,8 +7,87 @@ export class GalaxiesController extends BaseController {
         super('api/galaxies')
         this.router
             .get('', this.getAll)
-
+            .get('/:id', this.getById)
+            .get('/:id/stars', this.getGalaxyStars)
+            .get('/:id/planets', this.getGalaxyPlanets)
+            .get('/:id/', this.getGalaxyMoons)
+            .get('/:id', this.getGalaxySpecies)
+            .use(Auth0Provider.getAuthorizedUserInfo)
+            .post('', this.create)
+            .put('/:id', this.edit)
+            .delete('/:id', this.remove)
     }
 
+    async getAll(req, res, next) {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getById() {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getGalaxyStars(req, res, next) {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getGalaxyPlanets(req, res, next) {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getGalaxyMoons(req, res, next) {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getGalaxySpecies(req, res, next) {
+        try {
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async create(req, res, next) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async edit() {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async remove() {
+        try {
+
+        } catch (error) {
+
+        }
+    }
 
 }
